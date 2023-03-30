93eb52d1d9f87ab2ea07cbc64cb23ffdb00e138a
Janek123321 <janprobosz1@wp.pl>
2023-03-30 11:20:05 +0200

    first commit

---
+++ zmienne.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    double liczba1, liczba2; //wypisze wynik jako liczbe zmiennoprzecinkowa
+    cout<<"Dziele dwie liczby\n";
+    
+    cout<<"Podaj pierwsza liczbe:\n";
+    cin>>liczba1;
+
+    cout<<"Podaj druga liczbe:\n";
+    cin>>liczba2;
+
+    cout<<liczba1 <<"/" <<liczba2 <<" = " <<liczba1/liczba2;
+
+
+return 0;
+}
