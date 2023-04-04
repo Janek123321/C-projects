d18d023234db6505109851e2b0282e7af5d4387e
Janek123321 <janprobosz1@wp.pl>
2023-04-04 09:25:51 +0200

    third commit

---
+++ if.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+int liczba ;
+cout<<"podaj liczbę a powiem czy jest czy dodatnia ujemna"<<endl;
+cin>>liczba;
+cout << "Liczba = " << liczba << endl;
+string wb;
+// Zastosowanie instrukcji warunkowych:
+if (liczba > 0) wb = "dodatnia";
+if (liczba < 0) wb = "ujemna";//do ifa przyklejona jest tylko następna instrukcja
+cout << "Znak liczby: " << wb << endl;
+
+return 0;
+}
--- zadania.cpp
+++ zadania.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std;
+namespace pojemnosci {
+const float cola = 0.33;
+const float flaszka = 0.5;
+const float liter = 1;
+}
 
-int main() {
-   
-         
+int main()
+{
+
+    int a=10, b=20, c=5;
+    cout<<pojemnosci::cola*a+pojemnosci::flaszka*b+pojemnosci::liter*c<<endl;
 
 return 0;
 }
