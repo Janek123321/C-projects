e83a0c4d914bc53a226294cdf9b13a940e4a8c5c
Janek123321 <janprobosz1@wp.pl>
2023-04-13 12:52:24 +0200

    4/13/2023

--- if.cpp
+++ if.cpp
@@ -10,8 +10,10 @@ cin>>liczba;
 cout << "Liczba = " << liczba << endl;
 string wb;
 // Zastosowanie instrukcji warunkowych:
-if (liczba > 0) wb = "dodatnia";
-if (liczba < 0) wb = "ujemna";//do ifa przyklejona jest tylko następna instrukcja
+if (liczba > 0) 
+wb = "dodatnia";
+if (liczba < 0) 
+wb = "ujemna"; //do ifa przyklejona jest tylko następna instrukcja
 cout << "Znak liczby: " << wb << endl;
 
 return 0;
