87695a125b718380f276cea38b68f81106a4dc3d
Janek123321 <janprobosz1@wp.pl>
2023-05-30 08:53:15 +0200

    30/5/2023

---
+++ zadanie_switch.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int lekcje;
+    // cout << "Podaj numer lekcji a napisze ci jaka masz teraz lekcje: " << endl;
+    // cin >> lekcje;
+    // switch (lekcje)
+    // {
+    // case 1:
+    //     cout << "Masz teraz biologie\n";
+    //     break;
+    // case 2:
+    //     cout << "Masz teraz chemie\n";
+    //     break;
+    // case 3:
+    //     cout << "Masz teraz fizyke\n";
+    //     break;
+    // case 4:
+    //     cout << "Masz teraz matematyke\n";
+    //     break;
+    // case 5:
+    //     cout << "Masz teraz praktyke\n";
+    //     break;
+    // case 6:
+    //     cout << "Masz teraz praktyke\n";
+    //     break;
+    // default:
+    //     cout<<"Nie masz lekcji";
+    // }
+
+    int koniec;
+    cout << "Podaj numer lekcji a napisze Ci ile lekcji masz do konca: \n";
+    cin >> koniec;
+    switch (koniec)
+    {
+    case 1:
+        cout << "do konca zostaly ci 6 lekcje";
+        break;
+    case 2:
+        cout << "do konca zostaly ci 5 lekcje";
+        break;
+    case 3:
+        cout << "do konca zostaly ci 4 lekcje";
+        break;
+    case 4:
+        cout << "do konca zostaly ci 3 lekcje";
+        break;
+    case 5:
+        cout << "do konca zostaly ci 2 lekcje";
+        break;
+    case 6:
+        cout << "do konca zostaly ci 1 lekcje";
+        break;
+
+    default:
+        cout<<"Nie masz lekcji";
+        break;
+    }
+
+    return 0;
+}
